52ca2e58f576962e997e08a88fa3c876f27b3094
Segunwed <123121882+Segunwed@users.noreply.github.com>
2025-11-12 10:07:55 +0100

    Update app.py

--- AlmereBot/app.py
+++ AlmereBot/app.py
@@ -219,9 +219,9 @@ CONVERSATIONAL_QUESTIONS = [
         "options": [
             "04:00 AM - 07:00 AM (Early Morning)",
             "07:00 AM - 09:00 AM (Morning Peak)",
-            "09:00 AM - 04:00 PM (Midday)",
-            "04:00 PM - 08:00 PM (Evening Peak)",
-            "08:00 PM - 04:00 AM (Late Night/Overnight)"
+            "09:00 AM - 04:00 PM (Dat Time Period)",
+            "04:00 PM - 06:00 PM (Evening Peak)",
+            "06:00 PM - 04:00 AM (Evening/Overnight)"
         ]
     },
     {
